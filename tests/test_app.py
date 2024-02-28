7fbe81f63a12f4ba5afff8fa59df24dfa82cf5bf
Chiara daSilva Santos <cd58@njit.edu>
2024-02-27 20:42:47 -0500

    homework5

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,56 @@
+"""
+Module: test_app.py
+This module contains unit tests for the App class.
+"""
+
+import multiprocessing  # Import the multiprocessing module
 import pytest
 from app import App
 
+def test_app_get_environment_variable():
+    """Function to test environment variable"""
+    app = App()
+    # Retrieve the current environment setting
+    current_env = app.get_environment_variable('ENVIRONMENT')
+    # Assert that the current environment is what you expect
+    assert current_env in ['DEVELOPMENT', 'TESTING', 'PRODUCTION'], f"Invalid ENVIRONMENT: {current_env}"
+
+def start_app(input_queue):
+    """Function to start the App instance in a separate process."""
+    app = App()
+    app.command_handler.execute_command = lambda _: input_queue.get()  # Override execute_command to read from input_queue
+    app.start()
+
+def test_multiprocessing():
+    """Test that the application runs in multiple processes."""
+    input_queue = multiprocessing.Queue()
+
+    process = multiprocessing.Process(target=start_app, args=(input_queue,))
+    process.start()
+
+    # Simulate user input by putting 'exit' into the input_queue
+    input_queue.put('exit')
+
+    process.join(timeout=2)
+
+    assert not process.is_alive()
+
+
 def test_app_start_exit_command(capfd, monkeypatch):
     """Test that the REPL exits correctly on 'exit' command."""
-    # Simulate user entering 'exit'
+    app = App()
     monkeypatch.setattr('builtins.input', lambda _: 'exit')
-    App.start()
-    out, err = capfd.readouterr()
-
-    # Check that the initial greeting is printed and the REPL exits gracefully
-    assert "Hello World. Type 'exit' to exit." in out
-    assert "Exiting..." in out
+    with pytest.raises(SystemExit) as exc_info:
+        app.start()
+    assert exc_info.type == SystemExit
+    assert str(exc_info.value) == "Exiting..."
 
 def test_app_start_unknown_command(capfd, monkeypatch):
     """Test how the REPL handles an unknown command before exiting."""
-    # Simulate user entering an unknown command followed by 'exit'
+    app = App()
     inputs = iter(['unknown_command', 'exit'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    App.start()
-    out, err = capfd.readouterr()
-
-    # Check that the REPL responds to an unknown command and then exits after 'exit' command
-    assert "Hello World. Type 'exit' to exit." in out
-    assert "Unknown command. Type 'exit' to exit." in out
-    assert "Exiting..." in out
+    with pytest.raises(SystemExit) as exc_info:
+        app.start()
+    assert exc_info.type == SystemExit
+    assert str(exc_info.value) == "Exiting..."
